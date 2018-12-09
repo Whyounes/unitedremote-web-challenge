38293075bd53b68a0f1dd3e264a62ac51dce1b63
RAFIE Younes <younes.rafie@gmail.com>
2018-12-09 15:48:17 +0100

    Scaffold auth

---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    const TABLE = 'users';
+
+    protected $table = self::TABLE;
+
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
