2fef237f38ec9001151fdb97425d09d5ee9c7108
RAFIE Younes <younes.rafie@gmail.com>
2018-12-11 11:16:48 +0100

    Fix auth redirect & routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:api')
-    ->namespace('\\App\\Http\\Controllers\\Api\\')
-    ->group(function () {
-        Route::post('/shops/{shop}/like', 'ShopsController@likeShop');
-        Route::delete('/shops/{shop}/like', 'ShopsController@deleteLikeShop');
-
-        Route::post('/shops/{shop}/dislike', 'ShopsController@dislikeShop');
-
-        Route::get('/me/shops/liked', 'UsersController@likedShops');
-        Route::get('/me/shops/disliked', 'UsersController@dislikedShops');
-    });
-
-Route::get('/shops', 'Api\\ShopsController@home');
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth')
+    ->namespace('\\App\\Http\\Controllers\\Api\\')
+    ->name('api.')
+    ->group(function () {
+        Route::post('/shops/{shop}/like', 'ShopsController@likeShop')->name('shops.like');
+        Route::delete('/shops/{shop}/like', 'ShopsController@deleteLikeShop')->name('shops.like.delete');;
+
+        Route::post('/shops/{shop}/dislike', 'ShopsController@dislikeShop')->name('shops.dislike');;
+
+        Route::get('/me/shops/liked', 'UsersController@likedShops')->name('shops.liked.index');
+        Route::get('/me/shops/disliked', 'UsersController@dislikedShops')->name('shops.disliked.index');
+    });
+
+Route::get('/shops', 'Api\\ShopsController@home')->name('api.shops.index');
